b5b1613eb85a57f1211d2e87391767e9f247648e
divyajeettt <divyajeet21529@iiitd.ac.in>
2023-02-16 03:09:10 +0530

    Completed Question-2

--- Assignment-1/utils.py
+++ Assignment-1/utils.py
@@ -62,22 +62,29 @@ class KMeans:
 
     def fit(self, x_train: np.ndarray):
         self.x_train = x_train
+        self.n = self.x_train.shape[0]
         self.curr_centroids = np.random.rand(self.k, self.x_train.shape[1])
         self.prev_centroids = np.zeros(self.curr_centroids.shape)
-        self.curr_labels = np.zeros(self.x_train.shape[0])
+        self.curr_labels = np.zeros(self.n)
         self.prev_labels = np.zeros(self.curr_labels.shape)
 
     def train(self):
         iters = 0
-        while iters < self.max_iters or not np.allclose(self.prev_centroids, self.curr_centroids, atol=self.tol):
-            for i in range(self.x_train.shape[0]):
+        while not self.termination(iters):
+            for i in range(self.n):
                 distances = np.linalg.norm(self.curr_centroids - self.x_train[i], axis=1)
                 self.curr_labels[i] = np.argmin(distances)
             iters += 1
-            self.prev_centroids = self.curr_centroids
-            self.update_centroids()
-            self.prev_labels = self.curr_labels
+            self.update_params()
         return self.curr_centroids, self.curr_labels
+
+    def termination(self, iters: int):
+        return iters >= self.max_iters and np.allclose(self.prev_centroids, self.curr_centroids, atol=self.tol)
+
+    def update_params(self):
+        self.prev_centroids = self.curr_centroids
+        self.update_centroids()
+        self.prev_labels = self.curr_labels
 
     def update_centroids(self):
         for i in range(self.k):
@@ -92,11 +99,12 @@ class SilhouetteAnalysis:
     def fit(self, kmeans: KMeans):
         self.kmeans = kmeans
         self.x_train = self.kmeans.x_train
+        self.n = self.kmeans.n
         self.labels = self.kmeans.curr_labels
 
     def silhouette_scores(self):
-        s = np.zeros(self.x_train.shape[0])
-        for i in range(self.x_train.shape[0]):
+        s = np.zeros(self.n)
+        for i in range(self.n):
             a = self.a(i)
             b = self.b(i)
             s[i] = (b - a) / max(a, b)
@@ -110,7 +118,52 @@ class SilhouetteAnalysis:
     def b(self, i: int):
         return np.min([np.mean([np.linalg.norm(self.x_train[i] - sample) for sample in self.x_train[self.labels == j]]) for j in range(self.kmeans.k) if j != self.labels[i]])
 
-    def clusterwise_scores(self):
-        self.silhouette_scores()
+    def mean_sample_score(self, calculate: bool = False):
+        if calculate:
+            self.silhouette_scores()
+        return self.mean_score
+
+    def clusterwise_scores(self, calculate: bool = False):
+        if calculate:
+            self.silhouette_scores()
         self.cluster_scores = np.array([np.mean(self.sample_scores[self.labels == i]) for i in range(self.kmeans.k)])
         return self.cluster_scores
+
+
+class FuzzyCMeans(KMeans):
+    def __init__(self, c: int, m: float = 2, max_iters: int = 100, tol: float = 1e-5):
+        super().__init__(c, max_iters, tol)
+        self.c = c
+        self.m = m
+
+    def fit(self, x_train: np.ndarray):
+        super().fit(x_train)
+        self.curr_membership = np.random.rand(self.n, self.c)
+        self.prev_membership = np.zeros(self.curr_membership.shape)
+
+    def update_params(self):
+        super().update_params()
+        self.prev_membership = self.curr_membership
+        self.update_membership()
+
+    def update_membership(self):
+        for i in range(self.n):
+            for j in range(self.c):
+                x = self.x_train[i]
+                cj = self.curr_centroids[j]
+                self.curr_membership[i, j] = 1 / np.sum([np.linalg.norm(x - cj) / np.linalg.norm(x - ck) for ck in self.curr_centroids])
+                self.curr_membership[i, j] **= (2 / (self.m - 1))
+
+    def update_centroids(self):
+        for j in range(self.c):
+            total = weighted = 0
+            for i in range(self.n):
+                total += self.curr_membership[i, j] ** self.m
+                weighted += self.curr_membership[i, j] ** self.m * self.x_train[i]
+            self.curr_centroids[j] = weighted / total
+
+    def termination(self, iters: int):
+        return iters >= self.max_iters and np.allclose(self.prev_membership, self.curr_membership, atol=self.tol)
+
+    def objective(self):
+        return np.sum([np.linalg.norm(self.x_train[i] - self.curr_centroids[j]) ** 2 * self.curr_membership[i, j] ** self.m for i in range(self.n) for j in range(self.c)])
