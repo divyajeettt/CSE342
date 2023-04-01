cf679510c8ba30b3e12c046b7db5efe13ae1437a
divyajeettt <divyajeet21529@iiitd.ac.in>
2023-04-01 16:45:54 +0530

    Clubbed Question-4 and Question-5 + Finished Question-5

--- Assignment-2/utils.py
+++ Assignment-2/utils.py
@@ -60,29 +60,33 @@ class LinearRegression:
 
 class LogisticRegression:
     """
-    Implements the Logistic Regression algorithm for binary classification.
+    Implements the (Multinomial) Logistic Regression algorithm for n-ary classification.
     :attrs:
         x_train: The dataset to be used for the algorithm.
         y_train: The labels of the dataset.
         alpha: The learning rate.
         tol: Tolerance level for convergence.
         max_iters: The maximum number of iterations in which the clustering can be done.
+        weights: The learned weights of the model.
     """
 
     def __init__(self, x_train: np.ndarray, y_train: np.ndarray, alpha: float = 0.01, tol: float=1e-3, max_iters: int=100):
-        self.x_train = x_train
+        self.x_train = np.insert(x_train, 0, 1, axis=1)
         self.y_train = y_train
         self.alpha = alpha
         self.tol = tol
         self.max_iters = max_iters
-        self.weights = np.random.rand(x_train.shape[1])
+        if len(self.y_train.shape) > 1:
+            self.weights = np.random.rand(self.x_train.shape[1], self.y_train.shape[1])
+        else:
+            self.weights = np.random.rand(self.x_train.shape[1])
 
     def sigmoid(self, x: np.ndarray):
         """
         Returns the sigmoid at a point.
         :param x: The point at which the sigmoid is to be calculated.
         """
-        return 1 / (1 + np.exp(-self.weights.T @ x.T))
+        return 1 / (1 + np.exp(-x @ self.weights))
 
     def train(self):
         """
@@ -91,7 +95,7 @@ class LogisticRegression:
         iters = 0
         while iters < self.max_iters:
             old = self.weights.copy()
-            self.weights -= self.alpha * (self.sigmoid(self.x_train) - self.y_train) @ self.x_train
+            self.weights -= self.alpha * (self.x_train.T @ (self.sigmoid(self.x_train) - self.y_train))
             iters += 1
             if np.linalg.norm(self.weights - old) < self.tol:
                 break
@@ -101,7 +105,10 @@ class LogisticRegression:
         Returns the prediction of a point/data.
         :param x: The point/data to be predicted.
         """
-        return self.sigmoid(x) >= 0.5
+        if len(self.y_train.shape) > 1:
+            return np.argmax(self.sigmoid(np.insert(x, 0, 1, axis=1)), axis=1)
+        else:
+            return self.sigmoid(np.insert(x, 0, 1, axis=1)) >= 0.5
 
     def accuracy(self, x_test: np.ndarray, y_test: np.ndarray):
         """
@@ -126,6 +133,7 @@ class FDA:
         self.labels = labels
         self.means = np.array([data[labels == i].mean(axis=0) for i in np.unique(labels)])
         self.Sw = np.sum([np.cov(data[labels == i], rowvar=False) for i in np.unique(labels)], axis=0)
+        assert np.linalg.det(self.Sw) != 0
         self.Sb = np.cov(self.means, rowvar=False)
 
     def fisher_vector(self):
